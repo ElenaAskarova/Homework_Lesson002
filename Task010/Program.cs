8a7bba4e692fad78bd9a87b4c978975fde0acb8d
ElenaAskarova <askarova1205@mail.ru>
2022-11-23 16:09:16 +0500

    First_task

---
+++ Task010/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int amount = number.ToString().Length;
+if(amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    number = number / 10 % 10;
+    Console.WriteLine($"Вторая цифра введенного числа: {number}");
+}
