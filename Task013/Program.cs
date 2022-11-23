492fef91abcad4f803fe748438efa90d16820732
ElenaAskarova <askarova1205@mail.ru>
2022-11-23 18:16:04 +0500

    Third_Task

---
+++ Task013/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numS = Convert.ToString(num);
+int numDigit = numS.Length;
+if(numDigit > 2) Console.WriteLine($"Третья цифра введенного числа: {numS[2]}");
+else Console.WriteLine("Третьей цифры нет");
---
+++ Task015/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num >= 1 && num <= 7)
+{
+    if(num == 6 || num == 7) Console.WriteLine("Сегодня выходной");
+    else Console.WriteLine("Сегодня рабочий день");
+}
+else Console.WriteLine("Неверное число");
